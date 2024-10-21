af2bdd7fd7e6df9f7cdebf81701fc9498b9982fc
Sara Rubio <sara.rubio02@estudiant.upf.edu>
2024-10-21 12:55:31 +0200

    tercer intent

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,35 +1,33 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-import sys
-x = int (sys.argv [1])
 
 def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-	e = 0
-	b = 0
+	sum1 = 0
+	sum2 = 0
 	i = 1
 	y = int(x) - 1
 
 	while (i <= int(x)):
 		if (int(x) % i == 0):
-			e = e + 1
+			sum1 = sum1 + 1
 		i = i + 1
 
 	while (y >= 1):
 		i = 1
 		while (i <= y):
 			if (y % i == 0):
-				b = b + 1
+				sum2 = sum2 + 1
 			i = i + 1
-		if (b >= e):
+		if (sum2 >= sum1):
 			return("not anti-prime")
 			y = 0
 		else:
 			y = y - 1
-			b = 0
-	if (b < e):
+			sum2 = 0
+	if (sum2 < sum1):
 		return("anti-prime")
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
@@ -41,6 +39,8 @@ def main(x) :
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
+import sys
+x = int (sys.argv [1])
 
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
